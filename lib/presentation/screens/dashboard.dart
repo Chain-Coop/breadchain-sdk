8661da32f0146c1b920e4cfac2c385f9e1d6711e
The Full-Stack Jonin <francis94c@gmail.com>
2024-12-18 00:45:58 +0100

    feat: Groups

--- lib/presentation/screens/dashboard.dart
+++ lib/presentation/screens/dashboard.dart
@@ -17,94 +17,138 @@ class DashboardScreen extends StatelessWidget {
         body: Padding(
           padding:
               EdgeInsets.symmetric(horizontal: AppSpacing.horizontalSpacing),
-          child: Column(
-            children: [
-              AppSpacing.verticalSpaceSmall,
-              const ProfileCard(),
-              AppSpacing.verticalSpaceSmall,
-              Row(
-                children: [
-                  Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.only(
-                          left: AppSpacing.horizontalSpacing,
-                          right: AppSpacing.horizontalSpacing / 2),
-                      child: InkWell(
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: const Color(0xFFFFFDF0),
-                            borderRadius: BorderRadius.circular(12.r),
-                          ),
-                          child: Column(
-                            children: [
-                              AppSpacing.verticalSpaceMedium,
-                              SvgPicture.asset(
-                                'assets/svgs/plus.svg',
-                                package: 'breadchain_sdk',
-                                colorFilter: const ColorFilter.mode(
-                                  AppColors.primary,
-                                  BlendMode.srcIn,
-                                ),
-                                width: 40.w,
-                                height: 40.h,
-                              ),
-                              AppSpacing.verticalSpaceSmall,
-                              const Text("Create saving group"),
-                              AppSpacing.verticalSpaceSmall,
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.only(
-                          right: AppSpacing.horizontalSpacing,
-                          left: AppSpacing.horizontalSpacing / 2),
-                      child: InkWell(
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: const Color(0xFFFFFDF0),
-                            borderRadius: BorderRadius.circular(12.r),
-                          ),
-                          child: Padding(
-                            padding: EdgeInsets.only(
-                                left: AppSpacing.horizontalSpacing),
-                            child: Stack(
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                AppSpacing.verticalSpaceSmall,
+                const ProfileCard(),
+                AppSpacing.verticalSpaceSmall,
+                Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                            right: AppSpacing.horizontalSpacing / 2),
+                        child: InkWell(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: const Color(0xFFFFFDF0),
+                              borderRadius: BorderRadius.circular(12.r),
+                            ),
+                            child: Column(
                               children: [
-                                Positioned(
-                                  right: 0,
-                                  child: SvgPicture.asset(
-                                    'assets/svgs/history_large.svg',
-                                    package: 'breadchain_sdk',
-                                    width: 94.w,
-                                    height: 94.h,
+                                AppSpacing.verticalSpaceMedium,
+                                SvgPicture.asset(
+                                  'assets/svgs/plus.svg',
+                                  package: 'breadchain_sdk',
+                                  colorFilter: const ColorFilter.mode(
+                                    AppColors.primary,
+                                    BlendMode.srcIn,
                                   ),
+                                  width: 40.w,
+                                  height: 40.h,
                                 ),
-                                Column(
-                                  verticalDirection: VerticalDirection.down,
-                                  children: [
-                                    SizedBox(
-                                      height: 75.h,
-                                    ),
-                                    const Align(
-                                      alignment: Alignment.centerLeft,
-                                      child: Text("History"),
-                                    ),
-                                    AppSpacing.verticalSpaceSmall,
-                                  ],
-                                ),
+                                AppSpacing.verticalSpaceSmall,
+                                const Text("Create saving group"),
+                                AppSpacing.verticalSpaceSmall,
                               ],
                             ),
                           ),
                         ),
                       ),
                     ),
-                  )
-                ],
-              )
-            ],
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                            left: AppSpacing.horizontalSpacing / 2),
+                        child: InkWell(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: const Color(0xFFFFFDF0),
+                              borderRadius: BorderRadius.circular(12.r),
+                            ),
+                            child: Padding(
+                              padding: EdgeInsets.only(
+                                  left: AppSpacing.horizontalSpacing),
+                              child: Stack(
+                                children: [
+                                  Positioned(
+                                    right: 0,
+                                    child: SvgPicture.asset(
+                                      'assets/svgs/history_large.svg',
+                                      package: 'breadchain_sdk',
+                                      width: 94.w,
+                                      height: 94.h,
+                                    ),
+                                  ),
+                                  Column(
+                                    verticalDirection: VerticalDirection.down,
+                                    children: [
+                                      SizedBox(
+                                        height: 75.h,
+                                      ),
+                                      const Align(
+                                        alignment: Alignment.centerLeft,
+                                        child: Text("History"),
+                                      ),
+                                      AppSpacing.verticalSpaceSmall,
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                AppSpacing.verticalSpaceSmall,
+                Container(
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFFFFDF0),
+                    borderRadius: BorderRadius.circular(12.r),
+                  ),
+                  height: 95.h,
+                  width: double.infinity,
+                  child: Padding(
+                    padding:
+                        EdgeInsets.only(left: AppSpacing.horizontalSpacing),
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          right: 0,
+                          child: SvgPicture.asset(
+                            'assets/svgs/grafiti.svg',
+                            package: 'breadchain_sdk',
+                          ),
+                        ),
+                        Positioned(
+                          left: 0,
+                          bottom: 0,
+                          child: Padding(
+                            padding: EdgeInsets.only(bottom: 5.h),
+                            child: const Text(
+                              'My groups(2)',
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                AppSpacing.verticalSpaceMedium,
+                Text(
+                  "Active Groups",
+                  style: TextStyle(
+                    fontSize: 18.sp,
+                    fontWeight: FontWeight.w500,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
