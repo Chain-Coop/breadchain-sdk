b29746ba2dc687244dbb959b4053a7946011dfd6
The Full-Stack Jonin <francis94c@gmail.com>
2024-12-18 00:29:03 +0100

    feat: Create saving group button and history button

--- lib/presentation/screens/dashboard.dart
+++ lib/presentation/screens/dashboard.dart
@@ -1,6 +1,9 @@
+import 'package:breadchain_sdk/config/colors.dart';
 import 'package:breadchain_sdk/config/spacing.dart';
 import 'package:breadchain_sdk/presentation/widgets/profile_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class DashboardScreen extends StatelessWidget {
   static const routeName = '/';
@@ -18,6 +21,89 @@ class DashboardScreen extends StatelessWidget {
             children: [
               AppSpacing.verticalSpaceSmall,
               const ProfileCard(),
+              AppSpacing.verticalSpaceSmall,
+              Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                          left: AppSpacing.horizontalSpacing,
+                          right: AppSpacing.horizontalSpacing / 2),
+                      child: InkWell(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: const Color(0xFFFFFDF0),
+                            borderRadius: BorderRadius.circular(12.r),
+                          ),
+                          child: Column(
+                            children: [
+                              AppSpacing.verticalSpaceMedium,
+                              SvgPicture.asset(
+                                'assets/svgs/plus.svg',
+                                package: 'breadchain_sdk',
+                                colorFilter: const ColorFilter.mode(
+                                  AppColors.primary,
+                                  BlendMode.srcIn,
+                                ),
+                                width: 40.w,
+                                height: 40.h,
+                              ),
+                              AppSpacing.verticalSpaceSmall,
+                              const Text("Create saving group"),
+                              AppSpacing.verticalSpaceSmall,
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                          right: AppSpacing.horizontalSpacing,
+                          left: AppSpacing.horizontalSpacing / 2),
+                      child: InkWell(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: const Color(0xFFFFFDF0),
+                            borderRadius: BorderRadius.circular(12.r),
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.only(
+                                left: AppSpacing.horizontalSpacing),
+                            child: Stack(
+                              children: [
+                                Positioned(
+                                  right: 0,
+                                  child: SvgPicture.asset(
+                                    'assets/svgs/history_large.svg',
+                                    package: 'breadchain_sdk',
+                                    width: 94.w,
+                                    height: 94.h,
+                                  ),
+                                ),
+                                Column(
+                                  verticalDirection: VerticalDirection.down,
+                                  children: [
+                                    SizedBox(
+                                      height: 75.h,
+                                    ),
+                                    const Align(
+                                      alignment: Alignment.centerLeft,
+                                      child: Text("History"),
+                                    ),
+                                    AppSpacing.verticalSpaceSmall,
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              )
             ],
           ),
         ),
